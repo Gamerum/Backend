94af9d52e42ab26a7f0ad6198e01389c2659e161
Mustafa Toprak <mtoprak.l203@gmail.com>
2024-07-14 12:06:58 +0300

    Create Entities #5

---
+++ src/main/java/com/gamerum/backend/entity/Community.java
@@ -0,0 +1,55 @@
+package com.gamerum.backend.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.util.HashSet;
+import java.util.Set;
+
+@Data
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "Community",
+        uniqueConstraints = {
+                @UniqueConstraint(columnNames = "title")
+        }
+)
+public class Community {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String description;
+
+    private String tags;
+    private long clickCount;
+
+    @OneToMany(mappedBy = "community", fetch = FetchType.LAZY)
+    private Set<CommunityMember> members = new HashSet<>();
+
+    @OneToMany(mappedBy = "community", fetch = FetchType.LAZY)
+    private Set<Post> posts = new HashSet<>();
+
+    @Setter(AccessLevel.NONE)
+    @Column(name = "created_at", nullable = false)
+    private LocalDateTime createdAt;
+
+    @Column(name = "created_by_profile_id", nullable = false)
+    private long createdBy;
+
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    @Column(name = "updated_by_profile_id")
+    private Long updatedBy;
+
+    public Community() {
+        createdAt = LocalDateTime.now();
+    }
+}
