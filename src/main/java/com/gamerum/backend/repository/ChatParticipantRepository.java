81c8932c820416593072dae40505242c103620c6
halilkeklik <halil@halilkeklik.com>
2024-07-15 12:02:59 +0300

    Create Chat Service #6

---
+++ src/main/java/com/gamerum/backend/repository/ChatParticipantRepository.java
@@ -0,0 +1,13 @@
+package com.gamerum.backend.repository;
+
+import com.gamerum.backend.entity.ChatParticipant;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface ChatParticipantRepository extends CrudRepository<ChatParticipant, Long> {
+    Optional<ChatParticipant> findByProfileId(Long profileID);
+    Optional<ChatParticipant> findByChatId(Long chatID);
+}
