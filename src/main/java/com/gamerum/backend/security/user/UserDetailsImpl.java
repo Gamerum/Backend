4b1aaa22b51de56dc237d41756e2ee6e0287986c
Mustafa Toprak <mtoprak.l203@gmail.com>
2024-07-15 21:48:11 +0300

    Security codes rewritten more cleanly and tokens will be checked for users' authorities #18

--- src/main/java/com/gamerum/backend/security/user/UserDetailsImpl.java
+++ src/main/java/com/gamerum/backend/security/user/UserDetailsImpl.java
@@ -5,8 +5,10 @@ import com.gamerum.backend.external.persistence.entity.User;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 import java.util.Objects;
@@ -21,20 +23,13 @@ public class UserDetailsImpl implements UserDetails {
     private Collection<? extends GrantedAuthority> authorities;
 
     public static UserDetailsImpl build(User user) {
-        // authorities should be granted when role system implemented !
-        return new UserDetailsImpl(user.getId(), user.getUsername(), user.getPassword(), List.of());
-    }
+//        List<GrantedAuthority> authorities = user.getRoles().stream()
+//                .map(SimpleGrantedAuthority::new)
+//                .collect(Collectors.toList());
 
-    @Override
-    public String getUsername() { return username; }
-    @Override
-    public boolean isAccountNonExpired() { return true; }
-    @Override
-    public boolean isAccountNonLocked() { return true; }
-    @Override
-    public boolean isCredentialsNonExpired() { return true; }
-    @Override
-    public boolean isEnabled() { return true; }
+        List<GrantedAuthority> authorities = new ArrayList<>();
+        return new UserDetailsImpl(user.getId(), user.getUsername(), user.getPassword(), authorities);
+    }
 
     @Override
     public boolean equals(Object o) {
