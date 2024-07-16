1d35f6e1d220f6772a4bae96f951fbc41cc96f7a
halilkeklik <halil@halilkeklik.com>
2024-07-16 22:23:27 +0300

    Create Community Service and handle merge conflict #8.

--- src/main/java/com/gamerum/backend/external/persistence/entity/CommunityMember.java
+++ src/main/java/com/gamerum/backend/external/persistence/entity/CommunityMember.java
@@ -9,12 +9,14 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 @Builder
 @Entity
-@Table(name = "CommunityMembers",
-        uniqueConstraints = {
-                @UniqueConstraint(columnNames = "title")
-        }
-)
+@Table(name = "CommunityMembers")
 public class CommunityMember {
+    public enum CommunityMemberRole {
+        OWNER,
+        MOD,
+        USER
+    }
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -27,11 +29,10 @@ public class CommunityMember {
     @JoinColumn(name = "community_id", nullable = false)
     private Community community;
 
-    @Column(name = "is_mod")
-    private boolean isMod;
 
-    @Column(name = "is_owner")
-    private boolean isOwner;
+    @Enumerated(EnumType.STRING)
+    @Column(name = "role", nullable = false)
+    private CommunityMemberRole role;
 
     @Setter(AccessLevel.NONE)
     @Column(name = "joined_at", nullable = false)
---
+++ src/main/java/com/gamerum/backend/usecase/service/Impl/CommunityMemberServiceImpl.java
@@ -0,0 +1,50 @@
+package com.gamerum.backend.usecase.service.Impl;
+
+import com.gamerum.backend.external.persistence.entity.CommunityMember;
+import com.gamerum.backend.external.persistence.repository.CommunityMemberRepository;
+import com.gamerum.backend.usecase.service.CommunityMemberService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class CommunityMemberServiceImpl implements CommunityMemberService {
+
+    @Autowired
+    private CommunityMemberRepository communityMemberRepository;
+    @Override
+    public CommunityMember createCommunityMember(CommunityMember communityMember) {
+        return communityMemberRepository.save(communityMember);
+    }
+
+    @Override
+    public CommunityMember getCommunityMember(Long id) {
+        Optional<CommunityMember> optional = communityMemberRepository.findById(id);
+        if (optional.isEmpty()) {
+            throw new RuntimeException();
+        }
+        return optional.get();
+    }
+
+    @Override
+    public List<CommunityMember> getCommunityMembers() {
+        return (List<CommunityMember>) communityMemberRepository.findAll();
+    }
+
+    @Override
+    public void deleteCommunityMember(Long id) {
+        communityMemberRepository.deleteById(id);
+    }
+
+    @Override
+    public List<CommunityMember> getCommunityMembersByCommunity(Long communityId) {
+        return communityMemberRepository.findByCommunityId(communityId);
+    }
+
+    @Override
+    public List<CommunityMember> getCommunityMembersByProfile(Long profileId) {
+        return communityMemberRepository.findByProfileId(profileId);
+    }
+}
