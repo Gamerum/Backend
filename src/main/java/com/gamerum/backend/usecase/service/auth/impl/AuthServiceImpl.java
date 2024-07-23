a62843faa1dae033353755f54825f549a4250246
halilkeklik <halil@halilkeklik.com>
2024-07-23 17:42:48 +0300

    Change dtos package. #56

--- src/main/java/com/gamerum/backend/usecase/service/auth/impl/AuthServiceImpl.java
+++ src/main/java/com/gamerum/backend/usecase/service/auth/impl/AuthServiceImpl.java
@@ -1,9 +1,9 @@
 package com.gamerum.backend.usecase.service.auth.impl;
 
-import com.gamerum.backend.domain.dto.auth.request.LoginRequestDTO;
-import com.gamerum.backend.domain.dto.auth.response.LoginResponseDTO;
-import com.gamerum.backend.domain.dto.auth.request.RegisterRequestDTO;
-import com.gamerum.backend.domain.dto.auth.response.RegisterResponseDTO;
+import com.gamerum.backend.adaptor.dto.auth.request.LoginRequestDTO;
+import com.gamerum.backend.adaptor.dto.auth.response.LoginResponseDTO;
+import com.gamerum.backend.adaptor.dto.auth.request.RegisterRequestDTO;
+import com.gamerum.backend.adaptor.dto.auth.response.RegisterResponseDTO;
 import com.gamerum.backend.external.persistence.relational.entity.Profile;
 import com.gamerum.backend.external.persistence.relational.entity.User;
 import com.gamerum.backend.external.persistence.relational.repository.UserRepository;
