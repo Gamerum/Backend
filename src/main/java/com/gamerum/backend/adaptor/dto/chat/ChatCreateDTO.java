54d49d4bf1a95ed0c135233c94a122bf112a5f1e
Mustafa Toprak <mtoprak.l203@gmail.com>
2024-07-24 12:36:21 +0300

    ChatCreateDTO profileIds changed to participantProfileIds

--- src/main/java/com/gamerum/backend/adaptor/dto/chat/ChatCreateDTO.java
+++ src/main/java/com/gamerum/backend/adaptor/dto/chat/ChatCreateDTO.java
@@ -8,6 +8,6 @@ import java.util.Set;
 @Getter
 @AllArgsConstructor
 public class ChatCreateDTO {
-    private Set<Long> profileIds;
+    private Set<Long> participantProfileIds;
     private Long creatorProfileId;
 }
