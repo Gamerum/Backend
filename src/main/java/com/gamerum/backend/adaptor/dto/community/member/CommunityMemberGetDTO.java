894b023d05a04c5883273cd7d6638f704698f35a
Mustafa Toprak <mtoprak.l203@gmail.com>
2024-07-24 14:18:59 +0300

    Community DTOs #41

---
+++ src/main/java/com/gamerum/backend/adaptor/dto/community/member/CommunityMemberGetDTO.java
@@ -0,0 +1,11 @@
+package com.gamerum.backend.adaptor.dto.community.member;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class CommunityMemberGetDTO {
+    private Long id;
+
+}
