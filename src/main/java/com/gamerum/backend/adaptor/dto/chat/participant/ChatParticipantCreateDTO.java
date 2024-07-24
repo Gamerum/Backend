acfffc28395916d67d706034fdb74db7f2373a16
Mustafa Toprak <mtoprak.l203@gmail.com>
2024-07-24 12:25:44 +0300

    ChatParticipant DTOs #41

---
+++ src/main/java/com/gamerum/backend/adaptor/dto/chat/participant/ChatParticipantCreateDTO.java
@@ -0,0 +1,12 @@
+package com.gamerum.backend.adaptor.dto.chat.participant;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class ChatParticipantCreateDTO {
+    private Long profileId;
+    private Long chatId;
+    private Long addedByProfileId;
+}
