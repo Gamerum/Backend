57ff1780b29a078abb48a813c6fa86119e077223
Mustafa Toprak <mtoprak.l203@gmail.com>
2024-07-24 15:01:35 +0300

    Post DTOs #41

---
+++ src/main/java/com/gamerum/backend/adaptor/dto/community/post/PostCreateDTO.java
@@ -0,0 +1,14 @@
+package com.gamerum.backend.adaptor.dto.community.post;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class PostCreateDTO {
+    private String title;
+    private String tag;
+    private String text;
+    private String communityId;
+    private String writerProfileId;
+}
---
+++ src/main/java/com/gamerum/backend/adaptor/dto/community/post/PostUpdateDTO.java
@@ -0,0 +1,14 @@
+package com.gamerum.backend.adaptor.dto.community.post;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class PostUpdateDTO {
+    private Long id;
+    private String title;
+    private String tag;
+    private String text;
+    private Long updaterId;
+}
