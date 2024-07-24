f0701976d35f09f2268ec6d480c34522442a265e
Mustafa Toprak <mtoprak.l203@gmail.com>
2024-07-24 14:36:08 +0300

    In ChatParticipant we can acces its id directly, so no need to add chat id and profile id to find the ChatParticipant entity.

--- src/main/java/com/gamerum/backend/adaptor/dto/chat/participant/ChatParticipantGetDTO.java
+++ src/main/java/com/gamerum/backend/adaptor/dto/chat/participant/ChatParticipantGetDTO.java
@@ -6,7 +6,7 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public class ChatParticipantGetDTO {
-    private Long profileId;
+    private Long id;
     private String nickname;
     private boolean isAdmin;
 }
