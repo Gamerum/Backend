2cf16e21ec9701eac6b71e365a0f87255010baee
halilkeklik <halil@halilkeklik.com>
2024-07-26 20:34:53 +0300

    Game search added alternative names. #42.

--- src/main/java/com/gamerum/backend/external/persistence/elasticsearch/document/GameDocument.java
+++ src/main/java/com/gamerum/backend/external/persistence/elasticsearch/document/GameDocument.java
@@ -19,17 +19,26 @@ public class GameDocument extends DocumentBase{
     @Id
     private String id;
     private String name;
+    private List<String> alternativeNames;
     private List<Integer> genreIds;
+
 
     // Factory method for JSON deserialization
     @JsonCreator
     public static GameDocument fromJson(@JsonProperty("id") String id,
                                         @JsonProperty("name") String name,
+                                        @JsonProperty("alternative_names") List<AlternativeName> alternativeNameEntities,
                                         @JsonProperty("genres") List<GenreEntity> genreEntities) {
+
+        List<String> alternativeNames = alternativeNameEntities == null ? null : alternativeNameEntities.stream()
+                .map(AlternativeName::getName)
+                .toList();
+
         List<Integer> genreIds = genreEntities == null ? null : genreEntities.stream()
                 .map(GenreEntity::getId)
                 .collect(Collectors.toList());
-        return new GameDocument(id, name, genreIds);
+
+        return new GameDocument(id, name, alternativeNames, genreIds);
     }
 
     @Override
@@ -40,5 +49,11 @@ public class GameDocument extends DocumentBase{
     @Data
     public static class GenreEntity {
         private int id;
+    }
+
+    @Data
+    public static class AlternativeName {
+        private long id;
+        private String name;
     }
 }
