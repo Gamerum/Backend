3e140d44b03e4e88df48d8043bea873d649925aa
halilkeklik <halil@halilkeklik.com>
2024-07-26 16:39:55 +0300

    Update Sync Listeners. #42.

--- src/main/java/com/gamerum/backend/adaptor/consumer/eventListener/elasticsearch/CommunitySyncListener.java
+++ src/main/java/com/gamerum/backend/adaptor/consumer/eventListener/elasticsearch/CommunitySyncListener.java
@@ -1,31 +1,45 @@
 package com.gamerum.backend.adaptor.consumer.eventListener.elasticsearch;
 
+import co.elastic.clients.elasticsearch.ElasticsearchClient;
+import co.elastic.clients.elasticsearch.core.DeleteRequest;
+import co.elastic.clients.elasticsearch.core.IndexRequest;
 import com.gamerum.backend.external.persistence.elasticsearch.document.CommunityDocument;
-import com.gamerum.backend.external.persistence.elasticsearch.repository.CommunityESRepository;
 import com.gamerum.backend.external.persistence.relational.entity.Community;
 import jakarta.persistence.PostPersist;
 import jakarta.persistence.PostRemove;
 import jakarta.persistence.PostUpdate;
 import org.springframework.stereotype.Component;
 
+import java.io.IOException;
+
 
 @Component
 public class CommunitySyncListener {
-    private final CommunityESRepository communityESRepository;
+    private final ElasticsearchClient elasticsearchClient;
 
-    public CommunitySyncListener(CommunityESRepository communityESRepository) {
-        this.communityESRepository = communityESRepository;
+    public CommunitySyncListener(ElasticsearchClient elasticsearchClient) {
+        this.elasticsearchClient = elasticsearchClient;
     }
 
     @PostPersist
     @PostUpdate
-    public void handleAfterSave(Community community) {
+    public void handleAfterSave(Community community) throws IOException {
         CommunityDocument document = new CommunityDocument(community);
-        communityESRepository.save(document);
+        IndexRequest<CommunityDocument> indexRequest = IndexRequest.of(i -> i
+                .index("community")
+                .id(document.getId())
+                .document(document));
+
+        elasticsearchClient.index(indexRequest);
     }
 
     @PostRemove
-    public void handleAfterDelete(Community community) {
-        communityESRepository.deleteById(community.getId().toString());
+    public void handleAfterDelete(Community community) throws IOException {
+        DeleteRequest deleteRequest = DeleteRequest.of(d -> d
+                .index("community")
+                .id(String.valueOf(community.getId()))
+        );
+
+        elasticsearchClient.delete(deleteRequest);
     }
 }
