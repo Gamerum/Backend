7204fecdcf8b5e70d3413acad1f46886196a7870
halilkeklik <halil@halilkeklik.com>
2024-07-26 16:01:00 +0300

    Added game search with genre filter. #42.

--- src/main/java/com/gamerum/backend/adaptor/dto/search/GameSearchFilter.java
+++ src/main/java/com/gamerum/backend/adaptor/dto/search/GameSearchFilter.java
@@ -1,16 +1,15 @@
 package com.gamerum.backend.adaptor.dto.search;
 
-import com.gamerum.backend.external.persistence.elasticsearch.document.GameDocument;
 import lombok.Getter;
 
 import java.util.List;
 
 @Getter
 public class GameSearchFilter extends SearchFilter{
-    private final List<GameDocument.Genre> genres;
+    private final List<Integer> genreIds;
 
-    public GameSearchFilter(String keyword, Integer page, Integer size, List<GameDocument.Genre> genres) {
+    public GameSearchFilter(String keyword, Integer page, Integer size, List<Integer> genreIds) {
         super(keyword, page, size);
-        this.genres = genres;
+        this.genreIds = genreIds;
     }
 }
