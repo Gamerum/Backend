fa1741450a2d2faa2214ba2d76220720f17e594e
halilkeklik <halil@halilkeklik.com>
2024-07-26 16:39:55 +0300

    Update Sync Listeners. #42.

--- src/main/java/com/gamerum/backend/adaptor/consumer/eventListener/elasticsearch/PostSyncListener.java
+++ src/main/java/com/gamerum/backend/adaptor/consumer/eventListener/elasticsearch/PostSyncListener.java
@@ -1,30 +1,44 @@
 package com.gamerum.backend.adaptor.consumer.eventListener.elasticsearch;
 
+import co.elastic.clients.elasticsearch.ElasticsearchClient;
+import co.elastic.clients.elasticsearch.core.DeleteRequest;
+import co.elastic.clients.elasticsearch.core.IndexRequest;
 import com.gamerum.backend.external.persistence.elasticsearch.document.PostDocument;
-import com.gamerum.backend.external.persistence.elasticsearch.repository.PostESRepository;
 import com.gamerum.backend.external.persistence.relational.entity.Post;
 import jakarta.persistence.PostPersist;
 import jakarta.persistence.PostRemove;
 import jakarta.persistence.PostUpdate;
 import org.springframework.stereotype.Component;
 
+import java.io.IOException;
+
 @Component
 public class PostSyncListener {
-    private final PostESRepository postESRepository;
+    private final ElasticsearchClient elasticsearchClient;
 
-    public PostSyncListener(PostESRepository postESRepository) {
-        this.postESRepository = postESRepository;
+    public PostSyncListener(ElasticsearchClient elasticsearchClient) {
+        this.elasticsearchClient = elasticsearchClient;
     }
 
     @PostPersist
     @PostUpdate
-    public void handleAfterSave(Post post) {
+    public void handleAfterSave(Post post) throws IOException {
         PostDocument document = new PostDocument(post);
-        postESRepository.save(document);
+        IndexRequest<PostDocument> indexRequest = IndexRequest.of(i -> i
+                .index("post")
+                .id(document.getId())
+                .document(document));
+
+        elasticsearchClient.index(indexRequest);
     }
 
     @PostRemove
-    public void handleAfterDelete(Post post) {
-        postESRepository.deleteById(post.getId().toString());
+    public void handleAfterDelete(Post post) throws IOException {
+        DeleteRequest deleteRequest = DeleteRequest.of(d -> d
+                .index("post")
+                .id(String.valueOf(post.getId()))
+        );
+
+        elasticsearchClient.delete(deleteRequest);
     }
 }
--- src/main/java/com/gamerum/backend/adaptor/consumer/eventListener/elasticsearch/ProfileSyncListener.java
+++ src/main/java/com/gamerum/backend/adaptor/consumer/eventListener/elasticsearch/ProfileSyncListener.java
@@ -1,33 +1,33 @@
 package com.gamerum.backend.adaptor.consumer.eventListener.elasticsearch;
 
+import co.elastic.clients.elasticsearch.ElasticsearchClient;
+import co.elastic.clients.elasticsearch.core.IndexRequest;
 import com.gamerum.backend.external.persistence.elasticsearch.document.ProfileDocument;
-import com.gamerum.backend.external.persistence.elasticsearch.repository.ProfileESRepository;
 import com.gamerum.backend.external.persistence.relational.entity.Profile;
 import jakarta.persistence.PostPersist;
 import jakarta.persistence.PostUpdate;
 import org.springframework.stereotype.Component;
 
+import java.io.IOException;
+
 @Component
 public class ProfileSyncListener {
-    private final ProfileESRepository profileESRepository;
 
-    public ProfileSyncListener(ProfileESRepository profileESRepository) {
-        this.profileESRepository = profileESRepository;
-    }
+    private final ElasticsearchClient elasticsearchClient;
 
-    @PostPersist
-    public void handleAfterSave(Profile profile) {
-        ProfileDocument document = new ProfileDocument(profile);
-        profileESRepository.save(document);
+    public ProfileSyncListener(ElasticsearchClient elasticsearchClient) {
+        this.elasticsearchClient = elasticsearchClient;
     }
 
     @PostUpdate
-    public void handleAfterUpdate(Profile profile) {
-        if (!profile.isActive()) {
-            profileESRepository.deleteById(profile.getId().toString());
-            return;
-        }
+    @PostPersist
+    public void handleAfterSave(Profile profile) throws IOException {
         ProfileDocument document = new ProfileDocument(profile);
-        profileESRepository.save(document);
+        IndexRequest<ProfileDocument> indexRequest = IndexRequest.of(i -> i
+                .index("profile")
+                .id(document.getId())
+                .document(document));
+
+        elasticsearchClient.index(indexRequest);
     }
 }
