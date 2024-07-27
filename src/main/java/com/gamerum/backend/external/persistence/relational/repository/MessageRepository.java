9b3562adcd56b9a9ed77d8d10238c921b96696d6
Mustafa Toprak <mtoprak.l203@gmail.com>
2024-07-27 17:51:46 +0300

    Message delete logic updated #60

--- src/main/java/com/gamerum/backend/external/persistence/relational/repository/MessageRepository.java
+++ src/main/java/com/gamerum/backend/external/persistence/relational/repository/MessageRepository.java
@@ -5,9 +5,12 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import javax.swing.text.html.Option;
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface MessageRepository extends CrudRepository<Message, Long> {
     List<Message> findByChatId(@Param("chat_id") Long chatID);
+    Optional<Message> findByIdAndChatId(Long id, Long chatId);
 }
