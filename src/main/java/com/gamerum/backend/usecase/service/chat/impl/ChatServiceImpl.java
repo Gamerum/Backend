2ccffa92116971e4ae59b82433515097ad158e03
Mustafa Toprak <mtoprak.l203@gmail.com>
2024-07-27 14:32:40 +0300

    NotFoundException implemented to ChatServiceImpl #60

--- src/main/java/com/gamerum/backend/usecase/service/chat/impl/ChatServiceImpl.java
+++ src/main/java/com/gamerum/backend/usecase/service/chat/impl/ChatServiceImpl.java
@@ -7,6 +7,7 @@ import com.gamerum.backend.external.persistence.relational.entity.Profile;
 import com.gamerum.backend.external.persistence.relational.repository.ChatParticipantRepository;
 import com.gamerum.backend.external.persistence.relational.repository.ChatRepository;
 import com.gamerum.backend.external.persistence.relational.repository.ProfileRepository;
+import com.gamerum.backend.usecase.exception.NotFoundException;
 import com.gamerum.backend.usecase.service.chat.ChatService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
@@ -36,22 +37,21 @@ public class ChatServiceImpl implements ChatService {
 
     @Override
     public Chat createChat(ChatCreateDTO chat) {
-        Optional<Profile> creator = profileRepository.findById(chat.getCreatorProfileId());
-        if (creator.isEmpty()) throw new RuntimeException();
-        Profile creatorProfile = creator.get();
+        Profile creator = profileRepository.findById(chat.getCreatorProfileId())
+                .orElseThrow(() -> new NotFoundException(Profile.class, "ID", chat.getCreatorProfileId()));
 
-        Chat newChat = chatRepository.save(new Chat(creatorProfile.getId()));
+        Chat newChat = chatRepository.save(new Chat(creator.getId()));
 
         //Set Chat Admin
-        ChatParticipant admin = chatParticipantRepository.save(new ChatParticipant(creatorProfile, newChat, true));
+        ChatParticipant admin = chatParticipantRepository.save(new ChatParticipant(creator, newChat, true));
         newChat.getParticipants().add(admin);
 
         //Set Chat Participants
         if (chat.getParticipantProfileIds() != null) {
             List<ChatParticipant> chatParticipants = chat.getParticipantProfileIds().stream()
-                    .map(id -> profileRepository.findById(id))
-                    .filter(Optional::isPresent)
-                    .map(profile -> new ChatParticipant(profile.get(),newChat,false))
+                    .map(id -> profileRepository.findById(id).orElseThrow(() ->
+                            new NotFoundException(Profile.class, "ID", id)))
+                    .map(profile -> new ChatParticipant(profile, newChat,false))
                     .toList();
             List<ChatParticipant> participants = (List<ChatParticipant>) chatParticipantRepository.saveAll(chatParticipants);
             newChat.getParticipants().addAll(participants);
