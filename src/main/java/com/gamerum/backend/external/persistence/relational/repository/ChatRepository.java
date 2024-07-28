2bdafa816988bd3ddbf94a8a302786542d60912c
halilkeklik <halil@halilkeklik.com>
2024-07-28 15:50:07 +0300

    Chat request security logic updated. #60

--- src/main/java/com/gamerum/backend/external/persistence/relational/repository/ChatRepository.java
+++ src/main/java/com/gamerum/backend/external/persistence/relational/repository/ChatRepository.java
@@ -1,15 +1,25 @@
 package com.gamerum.backend.external.persistence.relational.repository;
 
 import com.gamerum.backend.external.persistence.relational.entity.Chat;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface ChatRepository extends JpaRepository<Chat, Long> {
-    @Query("SELECT c FROM Chat c JOIN FETCH c.participants WHERE c.id = :id")
-    Optional<Chat> findByIdWithParticipants(Long id);
+    @Query("SELECT c FROM Chat c JOIN FETCH c.participants, c.messages WHERE c.id = :id")
+    Optional<Chat> findByIdWithParticipantsAndMessages(Long id);
+
+    @Query("SELECT DISTINCT c FROM Chat c " +
+            "JOIN FETCH c.participants p " +
+            "JOIN FETCH c.messages m " +
+            "WHERE c.id = :chatId AND p.profile.id = :profileId")
+    Optional<Chat> findByIdWithParticipantsAndMessagesAndProfileId(Long chatId, Long profileId);
+
+    @Query("SELECT c FROM Chat c JOIN c.participants p WHERE p.profile.id = :profileId")
+    List<Chat> findChatsByProfileId(Long profileId, Pageable pageable);
 }
