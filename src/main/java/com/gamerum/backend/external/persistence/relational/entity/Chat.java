fd337737c01b1be21e03fcef6346a0ef6af7c074
Mustafa Toprak <mtoprak.l203@gmail.com>
2024-07-28 23:10:55 +0300

    Accessing token from SecurityContext #70

--- src/main/java/com/gamerum/backend/external/persistence/relational/entity/Chat.java
+++ src/main/java/com/gamerum/backend/external/persistence/relational/entity/Chat.java
@@ -29,7 +29,7 @@ public class Chat {
     private Long creatorProfileId;
 
     @Setter(AccessLevel.NONE)
-    @Column(name = "created_at", nullable = false)
+    @Column(name = "created_at", nullable = false, updatable = false)
     private LocalDateTime createdAt;
 
     @Column(name = "updated_at")
