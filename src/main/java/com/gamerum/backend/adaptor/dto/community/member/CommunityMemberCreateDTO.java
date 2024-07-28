93e5bfecaf1c07ab723a86f4d5b51036c298fa18
halilkeklik <halil@halilkeklik.com>
2024-07-28 18:28:30 +0300

    CommunityMember Requests updated. #60

--- src/main/java/com/gamerum/backend/adaptor/dto/community/member/CommunityMemberCreateDTO.java
+++ src/main/java/com/gamerum/backend/adaptor/dto/community/member/CommunityMemberCreateDTO.java
@@ -6,7 +6,6 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public class CommunityMemberCreateDTO {
-    private Long profileId;
-    private Long communityId;
-    private Long joinedByProfileId;
+    private long profileId;
+    private long joinedByProfileId;
 }
--- src/main/java/com/gamerum/backend/external/persistence/relational/repository/CommunityMemberRepository.java
+++ src/main/java/com/gamerum/backend/external/persistence/relational/repository/CommunityMemberRepository.java
@@ -1,6 +1,8 @@
 package com.gamerum.backend.external.persistence.relational.repository;
 
 import com.gamerum.backend.external.persistence.relational.entity.CommunityMember;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
@@ -9,8 +11,8 @@ import java.util.List;
 import java.util.Optional;
 
 @Repository
-public interface CommunityMemberRepository extends CrudRepository<CommunityMember, Long> {
-    List<CommunityMember> findByProfileId(Long profileID);
-    List<CommunityMember> findByCommunityId(Long profileID);
+public interface CommunityMemberRepository extends JpaRepository<CommunityMember, Long> {
+    List<CommunityMember> findByCommunityId(Long profileID, Pageable pageable);
     Optional<CommunityMember> findByProfileIdAndCommunityId(Long profileID, Long communityID);
+    boolean existsByProfileIdAndCommunityId(Long profileID, Long communityID);
 }
