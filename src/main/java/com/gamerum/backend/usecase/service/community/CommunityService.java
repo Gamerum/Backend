470210136c5f7923dc2951f623c35614e4e540e6
halilkeklik <halil@halilkeklik.com>
2024-08-02 19:40:12 +0300

    Community tag system added. #77

--- src/main/java/com/gamerum/backend/usecase/service/community/CommunityService.java
+++ src/main/java/com/gamerum/backend/usecase/service/community/CommunityService.java
@@ -2,13 +2,16 @@ package com.gamerum.backend.usecase.service.community;
 
 import com.gamerum.backend.adaptor.dto.community.CommunityCreateDTO;
 import com.gamerum.backend.adaptor.dto.community.CommunityUpdateDTO;
+import com.gamerum.backend.adaptor.dto.community.post.CommunityUpdateTagsDTO;
 import com.gamerum.backend.external.persistence.relational.entity.Community;
 
 import java.io.IOException;
+import java.util.List;
 
 public interface CommunityService {
     Community  getCommunity(Long communityId);
     Community createCommunity(CommunityCreateDTO communityCreateDTO) throws IOException;
     Community updateCommunity(Long communityId, CommunityUpdateDTO communityUpdateDTO);
     void deleteCommunity(Long communityId);
+    List<String> updateTagsToCommunity(Long communityId, CommunityUpdateTagsDTO communityUpdateTagsDTO);
 }
